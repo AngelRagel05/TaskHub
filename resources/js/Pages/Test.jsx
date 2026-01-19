5868661eca3302a95b77d6e524c1b8ca2f537437
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2026-01-19 19:12:12 +0100

    Bateria Ejercicios: 2. Trabajando con rutas

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,7 @@
+export default function Test() {
+    return (
+        <div className="p-4 text-secondary text-xl-center">
+            Página de prueba
+        </div>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+
 
 // Memoria Temporal4
 
@@ -43,7 +45,6 @@ Route::post('/crear-tarea', function (Request $request) {
 });
 
 // Listar Tareas
-
 Route::get('/tasks', function () {
 
     $tareas = json_decode(Storage::get('tareas.json'), true);
@@ -57,6 +58,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/test', function () {
+    return Inertia::render('Test');
 });
 
 require __DIR__ . '/auth.php';
