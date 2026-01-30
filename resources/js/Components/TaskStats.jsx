650e65bd58052e40e78f55a0e0bcddff996a88cc
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2026-01-30 10:15:44 +0100

    Añadido TaskStats.jsx con bloques de estadísticas

--- resources/js/Components/TaskStats.jsx
+++ resources/js/Components/TaskStats.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const TaskStats = () => {
-  return <div></div>;
+const TaskStats = ({ total, completed, progress }) => {
+    return (
+        <div className="flex justify-between mb-4 p-2 bg-gray-100 rounded shadow">
+            <div className="text-center">
+                <p className="font-bold">Total</p>
+                <p>{total}</p>
+            </div>
+            <div className="text-center">
+                <p className="font-bold">Completadas</p>
+                <p>{completed}</p>
+            </div>
+            <div className="text-center">
+                <p className="font-bold">Progreso</p>
+                <p>{progress}%</p>
+            </div>
+        </div>
+    );
 };
 
 export default TaskStats;
